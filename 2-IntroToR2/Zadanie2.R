8e0f198484a5c80b8e66af4842edce0523571fc9
SJ2137 <sylwia124657@gmail.com>
2024-10-15 11:57:58 +0200

    Add files via upload

---
+++ 2-IntroToR2/Zadanie2.R
@@ -0,0 +1,42 @@
+## Zadanie 2: Instalacja i użycie pakietów
+
+**Cel:** Nauka instalowania i używania pakietów w R
+
+### Instrukcje i rozwiązanie:
+
+1.  **Zainstaluj pakiet `ggplot2`.**
+  
+  ``` r
+install.packages("ggplot2")
+```
+
+2.  **Załaduj pakiet.**
+  
+  ``` r
+library(ggplot2)
+```
+
+3.  **Sprawdź dokumentację pakietu.**
+  
+  ``` r
+?ggplot2
+```
+
+4.  **Zainstaluj i załaduj** dodatkowy pakiet, który Cię zainteresuje. Listę pakietów w repozytorium CRAN wraz z opisami znajdziesz tutaj: <https://cran.r-project.org/web/packages/available_packages_by_name.html>
+  
+  ``` r
+install.packages("plotly")
+library(plotly)
+```
+
+5.  **Napisz krótki opis** w komentarzu.
+
+``` r
+# Pakiet 'plotly' służy do tworzenia interaktywnych wykresów.
+# Zainteresował mnie, ponieważ umożliwia tworzenie dynamicznych wizualizacji danych.
+```
+
+#Pakiet "mthapower" służy do obliczania wielkości próbki mitochondrialnego DNA i obliczania minimalnego rozmiaru, aby był istotny.
+#Wybrałam ten pakiet, ponieważ może mi się przydać do pracy magisterskiej
+
+6.  **Zapisz skrypt** jako `zadanie2.R` i prześlij go do swojego repozytorium.
